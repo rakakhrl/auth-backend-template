6615cf8856de90c01219c0540e434e084815ede6
rakakhrl <raka.khairil@gmail.com>
2026-05-18 10:05:31 +0700

    feat: add basic auth capability

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+export const auth = betterAuth({
+  secret: "Dl8EDKjAD8B6uJciLHoU8SeP3OWKIJsj",
+  baseURL: "http://localhost:4100",
+  trustedOrigins: ["http://localhost:3100"],
+  database: new Pool({
+    connectionString: "postgresql://postgres:sciadmin88@localhost:5432/auth-db",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
